5db21639074a80455377babea5a4536eb0a56bd9
CuongPM2022 <nhatnam2072013@gmail.com>
2023-12-08 19:05:29 +0700

    8/12/2023 Add some question for interview

--- app/script.js
+++ app/script.js
@@ -1,7 +1,7 @@
 const $ = (selector, parent = document) => parent.querySelector(selector); 
 const $$ = (selector, parent = document) => parent.querySelectorAll(selector);
 const sources = {
-	'interview': 10,
+	'interview': 13,
 	'family': 6,
 	'health': 6,
 	'hobby': 3,
